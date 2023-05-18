b35f4d16a7d7eb73a8054ac8d61e2f84c8937b31
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2023-05-18 09:11:30 +0530

    Update Introduction to Trees.cpp

--- Binary Tree/Traversals/Introduction to Trees.cpp
+++ Binary Tree/Traversals/Introduction to Trees.cpp
@@ -1,7 +1,10 @@
 // https://practice.geeksforgeeks.org/problems/introduction-to-trees/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=introduction-to-trees
-class Solution {
-  public:
-    int countNodes(int i) {
-        return pow(2,i-1);
+
+class Solution
+{
+public:
+    int countNodes(int i)
+    {
+        return pow(2, i - 1);
     }
 };
